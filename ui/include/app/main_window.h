ce896149fd179dd840241ea441d4ec1d42a145ac
Denys <feed.exfoss@gmail.com>
2025-12-19 23:50:34 +0200

    add ui for random data gen language/theme switcher

--- ui/include/app/main_window.h
+++ ui/include/app/main_window.h
@@ -1,58 +1,79 @@
 #pragma once
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
 
 #include <QMainWindow>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
 #include <QLineEdit>
 #include <QPushButton>
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QGridLayout>
-#include <QMessageBox>
-#include <QGroupBox>
 #include <QScrollArea>
-#include <QFont>
-#include <QDebug>
 #include <QTimer>
-#include <QStyle>
+#include <QMessageBox>
 #include <QDoubleValidator>
-#include <stdexcept>
-#include <map>
+#include <QStyle>
 #include <memory>
+#include <map>
+#include <vector>
 #include <optional>
+#include <random>
+
 #include "python_bridge.h"
+#include "model_info.h"
 
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
 public:
   explicit MainWindow(QWidget *parent = nullptr);
-  ~MainWindow() = default;
 
 private slots:
   void onPredictClicked();
   void onClearClicked();
   void onPythonError(const QString& error);
 
+
+  void onThemeToggle();
+  void onLangToggle();
+  void onRandomData();
+
 private:
   void setupUi();
+  void setupToolbar();
+  void updateTheme();
+  void updateTexts();
   std::optional<std::vector<float>> validateAndCollect();
+
+  // Components
+  QWidget *central;
+  QVBoxLayout *mainLayout;
+  QHBoxLayout *headerLayout;
+  QHBoxLayout *buttonLayout;
+  QScrollArea *scroll;
+  QWidget *scrollWidget;
+  QGridLayout *grid;
+
+  QLabel *title;
+  QLabel *modelInfoLabel;
+  QLabel *resultLabel;
+
+  QPushButton *predictButton;
+  QPushButton *clearButton;
+
+  QPushButton *themeBtn;
+  QPushButton *langBtn;
+  QPushButton *randomBtn;
 
   std::unique_ptr<PythonBridge> bridge;
   ModelInfo modelInfo;
   std::map<std::string, QLineEdit*> inputFields;
-  QLabel* resultLabel;
-  QLabel* modelInfoLabel;
-  QPushButton* predictButton;
-  QPushButton* clearButton;
+  std::map<std::string, QLabel*> featureLabels;
 
-  QWidget* central;
-  QVBoxLayout* mainLayout;
-  QLabel* title;
-  QScrollArea* scroll;
-  QWidget* scrollWidget;
-  QGridLayout* grid;
-  QHBoxLayout* buttonLayout;
+  // State
+  bool isDarkTheme = false;
+  QString currentLang = "en";
 };
 
-#endif // MAINWINDOW_H
+#endif // MAIN_WINDOW_H
